694e4ff8c42a0f5976e6fbb441d47416195488a5
Uzivatel1 <jiri.kopecky777@seznam.cz>
2024-11-26 17:15:07 +0100

    Comment updated in Program.cs

--- ShoesShelf/Program.cs
+++ ShoesShelf/Program.cs
@@ -4,7 +4,7 @@ using ShoesShelf.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the DI container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
